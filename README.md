f7aef7c7cb403749e5e6787e4f1b163548e8e8e7
Koichi Nakashima <koichi@nksm.name>
2019-03-04 00:59:55 +0900

    Add demo

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-![preview](preview.png)
+<script src="https://asciinema.org/a/231346.js" id="asciicast-231346" async data-autoplay="true" data-cols="100" data-rows="25"></script>
 
 ## Features
 
---
+++ demo/script.sh
@@ -0,0 +1,71 @@
+#!/bin/sh
+
+#ghostplay silent
+SCRIPT_DIR=$(cd "$(dirname "${GP_SOURCE:-$0}")"; pwd)
+cd "$SCRIPT_DIR"
+ghostplay_cleanup_handler() {
+  cd "$SCRIPT_DIR"
+  if [ -d project ]; then
+    rm -rf project
+  fi
+}
+#ghostplay end
+
+# Create your project directory
+mkdir project
+cd project
+
+#ghostplay sleep 3
+
+# Initialize
+shellspec --init
+
+#ghostplay sleep 3
+
+# Write your first specfile (of course you can use your favorite editor)
+#ghostplay batch
+cat << 'HERE' > spec/hello_spec.sh
+Describe 'hello.sh'
+  . lib/hello.sh
+  Example 'hello'
+    When call hello shellspec
+    The output should equal 'Hello shellspec!'
+  End
+End
+HERE
+#ghostplay end
+
+#ghostplay sleep 3
+
+# Create lib/hello.sh
+mkdir lib
+#ghostplay batch
+cat << 'HERE' > lib/hello.sh
+hello() {
+  :
+}
+HERE
+#ghostplay end
+
+#ghostplay sleep 3
+
+# It goes fail because hello function not implemented.
+shellspec
+
+#ghostplay sleep 3
+
+# Write hello function (of course you can use your favorite editor)
+#ghostplay batch
+cat << 'HERE' > lib/hello.sh
+hello() {
+  echo "Hello ${1}!"
+}
+HERE
+#ghostplay end
+
+#ghostplay sleep 3
+
+# It goes success!
+shellspec
+
+# Done
